58d44e5aa705b1c9102fb0b78190accba9683585
Gayathri <kgayathri0111.com>
2025-06-14 18:25:56 +0530

    updated

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,14 +1,56 @@
 import { Button, TextField } from '@mui/material'
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 
-const Add = () => {
+const Add = (props) => {
+   var[emp,setemp]=useState({Name:"",Age:"",Dept:"",Salary:""})
+   var navigate=useNavigate()
+   var location = useLocation()
+   console.log("loc: ",location.state)
+
+   const inputHandler = (e) => {
+    setemp({...emp, [e.target.name]:e.target.value});
+    console.log(emp);
+   };
+
+   const addHandler=()=>{
+    if(location.state !==null){
+      axios.put("http://localhost:3004/update/"+location.state.val._id,emp)
+     .then((res)=>{
+      alert(res.data.message)
+      navigate("/view")
+    })
+  }
+    else{
+    axios.post("http://localhost:3004/add",emp)
+    .then((res)=>{
+      alert(res.data.message)
+      navigate("/view")
+    })
+   }
+   } 
+   useEffect(() =>{
+    if(location.state !== null){
+      setemp({
+        ...emp,
+        Name: location.state.val.Name,
+        Age: location.state.val.Age,
+        Dept: location.state.val.Dept,
+        Salary: location.state.val.Salary,
+      });
+    }
+   },[]);
+
   return (
     <div>
-        <br/><br/><TextField label="Name" variant="outlined" ></TextField><br/><br/>
-        <TextField label="Age" variant="outlined" ></TextField><br/><br/>
-        <TextField label="Dept" variant="outlined" ></TextField><br/><br/>
-        <TextField label="Salary" variant="outlined" ></TextField><br/><br/>
-        <Button variant="contained" >Add</Button>
+       
+        <br/><br/><h3>EMPLOYEES</h3><br/><br/>
+        <TextField label="Name" variant="outlined" name="Name" value={emp.Name} onChange={inputHandler}></TextField><br/><br/>
+          <TextField label="Age" variant="outlined"  name="Age" value={emp.Age} onChange={inputHandler}></TextField><br/><br/>
+        <TextField label="Dept" variant="outlined"  name="Dept" value={emp.Dept} onChange={inputHandler}></TextField><br/><br/>
+        <TextField label="Salary" variant="outlined"  name="Salary"value={emp.Salary} onChange={inputHandler}></TextField><br/><br/>
+        <Button variant="contained" color="secondary" onClick={addHandler} >SUBMIT</Button>
     </div>
   )
 }
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,7 +1,31 @@
-import { Table, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
-import React from 'react'
+import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const View = () => {
+    var[user,setuser]=useState([])
+    var navigate = useNavigate()
+
+
+    axios.get("http://localhost:3004/view")
+    .then((res)=>{
+        console.log(res.data)
+        setuser(res.data)
+    })
+    const delValue=(id)=>{
+        console.log(id)
+        axios.delete("http://localhost:3004/delete/"+id)
+        .then((res)=>{
+            alert(res.data.message)
+            //window.location.reload()
+        })
+    }
+    const updateValue=(val)=>{
+        navigate("/add",{ state:{ val } });
+        };
+    
   return (
     <div>
         <TableContainer>
@@ -12,8 +36,29 @@ const View = () => {
                         <TableCell sx={{ fontWeignt: 'bold' }}>Age</TableCell>
                         <TableCell sx={{ fontWeignt: 'bold' }}>Dept</TableCell>
                         <TableCell sx={{ fontWeignt: 'bold' }}>Salary</TableCell>
+
                     </TableRow>
                 </TableHead>
+                <TableBody>
+
+
+                        {user.map((val)=>{
+                        return(
+                    <TableRow>
+                        <TableCell>{val.Name} </TableCell>
+                        <TableCell>{val.Age}</TableCell>
+                        <TableCell>{val.Dept}</TableCell>
+                         <TableCell>{val.Salary}</TableCell>
+                         <TableCell>
+                            <Button variant="contained"
+                             onClick={()=>{updateValue(val)}} >UPDATE</Button>&nbsp; &nbsp;
+                            <Button variant="contained"
+                             onClick={()=>{delValue(val._id)}} >DELETE</Button>
+                         </TableCell>
+                    </TableRow>
+                    )
+                })}
+                </TableBody>
             </Table>
         </TableContainer>
     </div>
